8d2acd93e9a494180fcc68a28ff625945030e4bc
Stefan Dirix <sdirix@eclipsesource.com>
2021-03-09 08:45:24 +0100

    Initial Vue 2 seed

---
+++ README.md
@@ -0,0 +1,15 @@
+# JSON Forms Vue 2 Seed
+
+This seed demonstrates how to use [JSON Forms](https://jsonforms.io) with Vue in order to render a simple form for displaying a task entity.
+You can find the [Vue 3 seed on the `main` branch](https://github.com/eclipsesource/jsonforms-vue-seed).
+
+It is based on the `vue create` Hello World project.
+
+`src/App.vue` contains the JSON Forms specific code.
+
+- Execute `npm ci` to install the prerequisites.
+- Execute `npm run serve` to start the application.
+
+Browse to http://localhost:8080 to see the application in action.
+
+For more information please see the JSON Forms Vue [documentation](https://jsonforms.io/docs/integrations/vue).
---
+++ src/App.vue
@@ -0,0 +1,188 @@
+<template>
+  <div id="app">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+    <h1>JSON Forms Vue 2</h1>
+    <div class="myform">
+      <json-forms
+        v-bind:data="data"
+        v-bind:renderers="renderers"
+        v-bind:schema="schema"
+        v-bind:uischema="uischema"
+        @change="onChange"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+import { JsonForms, JsonFormsChangeEvent } from "@jsonforms/vue2";
+import {
+  defaultStyles,
+  mergeStyles,
+  vanillaRenderers
+} from "@jsonforms/vue2-vanilla";
+
+// mergeStyles combines all classes from both styles definitions
+const myStyles = mergeStyles(defaultStyles, { control: { label: "mylabel" } });
+
+const renderers = [
+  ...vanillaRenderers
+  // here you can add custom renderers
+];
+
+const schema = {
+  properties: {
+    name: {
+      type: "string",
+      minLength: 1,
+      description: "The task's name"
+    },
+    description: {
+      title: "Long Description",
+      type: "string"
+    },
+    done: {
+      type: "boolean"
+    },
+    dueDate: {
+      type: "string",
+      format: "date",
+      description: "The task's due date"
+    },
+    rating: {
+      type: "integer",
+      maximum: 5
+    },
+    recurrence: {
+      type: "string",
+      enum: ["Never", "Daily", "Weekly", "Monthly"]
+    },
+    recurrenceInterval: {
+      type: "integer",
+      description: "Amount of days until recurrence"
+    }
+  }
+};
+
+const uischema = {
+  type: "HorizontalLayout",
+  elements: [
+    {
+      type: "VerticalLayout",
+      elements: [
+        {
+          type: "Control",
+          scope: "#/properties/name"
+        },
+        {
+          type: "Control",
+          scope: "#/properties/description",
+          options: {
+            multi: true
+          }
+        },
+        {
+          type: "Control",
+          scope: "#/properties/done"
+        }
+      ]
+    },
+    {
+      type: "VerticalLayout",
+      elements: [
+        {
+          type: "Control",
+          scope: "#/properties/dueDate"
+        },
+        {
+          type: "Control",
+          scope: "#/properties/rating"
+        },
+        {
+          type: "Control",
+          scope: "#/properties/recurrence"
+        },
+        {
+          type: "Control",
+          scope: "#/properties/recurrenceInterval"
+        }
+      ]
+    }
+  ]
+};
+
+export default defineComponent({
+  name: "App",
+  components: {
+    JsonForms
+  },
+  data() {
+    return {
+      // freeze renderers for performance gains
+      renderers: Object.freeze(renderers),
+      data: {
+        name: "Send email to Adrian",
+        description: "Confirm if you have passed the subject\nHereby ...",
+        done: true,
+        recurrence: "Daily",
+        rating: 3
+      },
+      schema,
+      uischema
+    };
+  },
+  methods: {
+    onChange(event: JsonFormsChangeEvent) {
+      this.data = event.data;
+    }
+  },
+  provide() {
+    return {
+      styles: myStyles
+    };
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-left: 120px;
+  margin-right: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.logo {
+  flex: 0 1 auto;
+}
+
+.mylabel {
+  color: darkslategrey;
+}
+
+.vertical-layout {
+  width: 300px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.myform {
+  width: 640px;
+  margin: 0 auto;
+}
+
+.text-area {
+  min-height: 80px;
+}
+
+.control > .wrapper > input[type="checkbox"] {
+  flex: 0;
+}
+</style>
