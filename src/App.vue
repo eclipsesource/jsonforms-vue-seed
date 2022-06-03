94fa589207d6066bf3c5c34dfaf954c44dde670e
Štefan Černý <stefan.cerny@gmail.com>
2022-06-03 14:05:41 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <h1>JSON Forms Vue 3</h1>
   <div class="myform">
     <json-forms
@@ -28,87 +28,9 @@ const renderers = [
   ...vanillaRenderers,
   // here you can add custom renderers
 ];
-
-const schema = {
-  properties: {
-    name: {
-      type: "string",
-      minLength: 1,
-      description: "The task's name"
-    },
-    description: {
-      title: "Long Description",
-      type: "string",
-    },
-    done: {
-      type: "boolean",
-    },
-    dueDate: {
-      type: "string",
-      format: "date",
-      description: "The task's due date"
-    },
-    rating: {
-      type: "integer",
-      maximum: 5,
-    },
-    recurrence: {
-      type: "string",
-      enum: ["Never", "Daily", "Weekly", "Monthly"]
-    },
-    recurrenceInterval: {
-      type: "integer",
-      description: "Days until recurrence"
-    },
-  },
-};
-
-const uischema = {
-  type: "HorizontalLayout",
-  elements: [
-    {
-      type: "VerticalLayout",
-      elements: [
-        {
-          type: "Control",
-          scope: "#/properties/name",
-        },
-        {
-          type: "Control",
-          scope: "#/properties/description",
-          options: {
-            multi: true,
-          }
-        },
-        {
-          type: "Control",
-          scope: "#/properties/done",
-        },
-      ],
-    },
-    {
-      type: "VerticalLayout",
-      elements: [
-        {
-          type: "Control",
-          scope: "#/properties/dueDate",
-        },
-        {
-          type: "Control",
-          scope: "#/properties/rating",
-        },
-        {
-          type: "Control",
-          scope: "#/properties/recurrence",
-        },
-        {
-          type: "Control",
-          scope: "#/properties/recurrenceInterval",
-        },
-      ],
-    },
-  ],
-};
+// import "vue-form-json/dist/vue-form-json.css";
+import schema from "./forms/schema";
+import uischema from "./forms/uischema";
 
 export default defineComponent({
   name: "App",
@@ -153,23 +75,5 @@ export default defineComponent({
   margin-top: 60px;
   margin-left: 120px;
   margin-right: 120px;
-}
-
-.mylabel {
-  color: darkslategrey;
-}
-
-.vertical-layout {
-  margin-left: 10px;
-  margin-right: 10px;
-}
-
-.myform {
-  width: 640px;
-  margin: 0 auto;
-}
-
-.text-area {
-  min-height: 80px;
 }
 </style>
